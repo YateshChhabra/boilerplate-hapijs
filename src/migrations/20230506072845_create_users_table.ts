17480fe8b1c426cb9879b8d8493f8dcd7744c365
yatesh-vinove <yatesh.chhabra@mail.vinove.com>
2023-05-06 13:01:38 +0530

    make migration for users table and update package.json file with some scripts

---
+++ src/migrations/20230506072845_create_users_table.ts
@@ -0,0 +1,14 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('username').notNullable().unique();
+    table.string('password').notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists('users');
+}
